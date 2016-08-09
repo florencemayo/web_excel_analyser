e00938b21510d472563a0d9d5d06ff3142edd2e4
Brian Anderson <Brian Anderson>
2016-08-09 14:52:51 +0300

    location view along with the location controller and the edited the District and Region Controllers.

--- public/controllers/locationController.js
+++ public/controllers/locationController.js
@@ -2,6 +2,39 @@
  * Created by kelvin on 8/7/16.
  */
 var locationController = angular.module("locationController",[])
-    .controller('locationController', ['$scope', function ($scope) {
-        console.log("nimefika kwenye location hapa")
-    }]);
+    .controller('locationController', ['$scope','$http', function ($scope, $http) {
+        console.log("Location Controller Page!");
+
+        $scope.region;
+        $scope.district;
+
+        $scope.save = function(){
+          var url = 'http://localhost:8001/#location';
+          $http.post(url, {
+            nameRegion:$scope.region;
+
+          }).success(function (data, status, headers, config){
+            if (!data.error) {
+              console.log(data);
+            }
+            else console.log(data);
+          });
+
+        }
+
+        $scope.add = function(){
+          var url = 'http://localhost:8001/#location';
+          $http.post(url, {
+            nameDistrict:$scope.district;
+
+          }).success(function (data, status, headers, config){
+            if (!data.error) {
+              console.log(data);
+            }
+            else console.log(data);
+          });
+
+        }
+
+    }]
+  );
